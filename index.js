2b1703f20fd2d6b36acf971ba18d073e00840006
Mike <mike.munene@student.moringaschool.com>
2023-04-06 17:11:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  return word === word.split('').reverse().join('');
 }
 
 /* 
   Add your pseudocode here
+  * split the string then reverse the join the string and finally write a condition that checks whether the word is a palindrome
 */
+
 
 /*
   Add written explanation of your solution here
+  ? A function that returns true if a word is a palindrome else returns false
 */
 
 // You can run `node index.js` to view these console logs
